5f4da59a51f5c879866fd82f58ec0cc877503ba7
Dreamy-dengping <66986342+Dreamy-dengping@users.noreply.github.com>
2022-09-06 20:42:01 +0800

    feat：新增命令行交互

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const program = require('commander');
 
 const { resolveOptionbs } = require("./lib/core/hellp.js");
 const { createCommands } = require("./lib/core/createCommands.js");
+
 // 获取版本号(第二个参数可以自定义指令打印版本号的参数：例如：gnip --version或者gnip-v)
 program.version(require("./package.json").version, "-v,--version")
 
