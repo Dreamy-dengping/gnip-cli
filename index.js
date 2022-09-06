a06c5c3d28ad80aa59f9b3abaf4b48e5f40eb42a
Dreamy-dengping <66986342+Dreamy-dengping@users.noreply.github.com>
2022-09-06 16:22:03 +0800

    feat：初始化脚手架文件

---
+++ index.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+const program = require('commander');
+
+const { resolveOptionbs } = require("./lib/core/hellp.js");
+const { createCommands } = require("./lib/core/createCommands.js");
+// 获取版本号(第二个参数可以自定义指令打印版本号的参数：例如：gnip --version或者gnip-v)
+program.version(require("./package.json").version, "-v,--version")
+
+// 配置可选信息
+resolveOptionbs()
+// 创建指令
+createCommands()
+
+program.parse(process.argv);
+
